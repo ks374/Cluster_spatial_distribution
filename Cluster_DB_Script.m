3e298d0a6923e9828322f6d88c5b0f5e248278db
ks374 <chenghaz@terpmail.umd.edu>
2024-02-01 14:35:16 -0500

    Experiment_2.1 and Experiment 2.2

--- Cluster_DB_Script.m
+++ Cluster_DB_Script.m
@@ -10,3 +10,23 @@ DBP = DBP.splitter();
 DB_A = "Pos_single_DB";
 DB_B = "Pos_multi_DB";
 temp_ratios = DBP.batch_close_check(DB_A,DB_B,1.5);
+%%
+DB_A = "Pos_single_DB";
+DB_B = "Pos_single_DB";
+temp_ratios = DBP.batch_close_check(DB_A,DB_B,1.5);
+disp(temp_ratios);
+%%
+DBP = DBP.init_rand_DB();
+DBP = DBP.rpl_generator(1);
+%%
+DB_A = "Pos_single_DB_rand";
+DB_B = "Pos_single_DB_rand";
+temp_ratios = DBP.batch_close_check(DB_A,DB_B,1.5);
+disp(temp_ratios);
+%%
+DBP = DBP.init_rand_DB();
+DBP = DBP.rpl_generator(2);
+DB_A = "Pos_single_DB_rand";
+DB_B = "Pos_single_DB_rand";
+temp_ratios = DBP.batch_close_check(DB_A,DB_B,1.5);
+disp(temp_ratios);
--- DB_processor.m
+++ DB_processor.m
@@ -10,9 +10,36 @@ classdef DB_processor
         Neg_multi_DB table;
         Neg_single_DB table;
 
+        All_DB_rand table; 
+        Pos_DB_rand table;
+        Neg_DB_rand table;
+        Pos_multi_DB_rand table;
+        Pos_single_DB_rand table;
+        Neg_multi_DB_rand table;
+        Neg_single_DB_rand table;
+
         Sample_name_list = {'WTP2_A','WTP2_B','WTP2_C','WTP4_A','WTP4_B','WTP4_C',...
                 'WTP8_A','WTP8_B','WTP8_C','B2P2_A','B2P2_B','B2P2_C',...
                 'B2P4_A','B2P4_B','B2P4_C','B2P8_A','B2P8_B','B2P8_C'};
+        Image_folder_list = {'Y:\Chenghang\Backup_Raw_Data\1.2.2021_P2EA_B\',...
+            'Y:\Chenghang\Backup_Raw_Data\1.4.2021_P2EB_B\',...
+            'Y:\Chenghang\4_Color\Raw\1.6.2021_P2EC_B\',...
+            'Y:\Chenghang\Backup_Raw_Data\7.29.2020_P4EB\',...
+            'Y:\Chenghang\Backup_Raw_Data\9.25.2020_P4EC_B\',...
+            'Y:\Chenghang\Backup_Raw_Data\12.5.2020_P4ED_B\',...
+            'Y:\Chenghang\Backup_Raw_Data\12.21.2020_P8EA_B\',...
+            'Y:\Chenghang\4_Color\Raw\12.23.2020_P8EB_B\',...
+            'Y:\Chenghang\4_Color\Raw\1.12.2021_P8EC_B\',...
+            'Y:\Chenghang\Backup_Raw_Data\9.29.2020_B2P2A_B\',...
+            'Y:\Chenghang\4_Color\Raw\12.13.2020_B2P2B_B\',...
+            'Y:\Chenghang\Backup_Raw_Data\12.18.2020_B2P2C_B\',...
+            'Y:\Chenghang\Backup_Raw_Data\10.3.2020_B2P4A_B\',...
+            'Y:\Chenghang\Backup_Raw_Data\10.27.2020_B2P4B_B\',...
+            'Y:\Chenghang\Backup_Raw_Data\12.8.2020_B2P4C_B\',...
+            'Y:\Chenghang\Backup_Raw_Data\12.12.2020_B2P8A_B\',...
+            'Y:\Chenghang\4_Color\Raw\1.13.2021_B2P8B_B\',...
+            'Y:\Chenghang\4_Color\Raw\1.11.2021_B2P8C_B\'
+            };
         save_ID int16; %To save .mat files for experiments. 
     end
     
@@ -87,48 +114,98 @@ classdef DB_processor
             end
         end
 
-        function DoC = get_DoC(obj,Matrix_A,Matrix_B,thre,save_logical,outpath)
-            %Get DoC from two centeroid matrixes
-            Dist_mat = obj.Get_Dist_2_matrix(Matrix_A,Matrix_B,save_logical,outpath);
-            Dist_mat = Dist_mat < thre;
-            DoC = zeros(1,size(Dist_mat,1));
-            for i = 1:size(Dist_mat,1)
-                DoC(i) = numel(find(Dist_mat(i,:)));
-            end
+        function [num_images,Height,Width] = get_stack_info(obj,i)
+            inpath = obj.Image_folder_list{i};
+            inpath = [inpath 'analysis\Result\1_soma\'];
+            files = dir([inpath '*.tif']);
+            infos = imfinfo([inpath files(1).name]);
+            num_images = numel(files);
+            Height = infos.Height;
+            Width = infos.Width;
         end
-        function Batch_DoCs(obj,indata_A,indata_B,thre,outpath)
-            obj.save_ID = 1;
-            for i = 1:18
-                array_A = obj.get_array(indata_A,i,6:8);
-                array_B = obj.get_array(indata_B,i,6:8);
-                Cur_DoC = obj.get_DoC(array_A,array_B,thre,1,outpath);
-                writematrix(Cur_DoC,[outpath,'DoC.csv'],'WriteMode','append');
-                obj.save_ID = obj.save_ID + 1;
+        function soma_images = get_soma_mask(obj,i)
+            inpath = obj.Image_folder_list{i};
+            inpath = [inpath 'analysis\Result\1_soma\'];
+            files = dir([inpath '*.tif']);
+            [num_images,Height,Width] = obj.get_stack_info(i);
+            soma_images = zeros(Height,Width,num_images,'uint8');
+            parfor j = 1:num_images
+                soma_images(:,:,j) = imread([inpath,files(j).name]);
             end
+            soma_images = logical(soma_images);
         end
-
-        
-
-
-        function ratio = get_ratio(obj,Matrix_A,Matrix_B,thre,save_logical,outpath)
-            Dist_mat = obj.Get_Dist_2_matrix(Matrix_A,Matrix_B,save_logical,outpath);
-            Dist_mat = Dist_mat < thre;
-            count_all = size(Dist_mat,1);
-            count_nearby = 0;
-            for i = 1:size(Dist_mat,1)
-                if Dist_mat(i,1) == 1
-                    count_nearby = count_nearby + 1;
+        function rand_position_list = get_rand_positions(obj,i,num)
+            [num_images,Height,Width] = obj.get_stack_info(i);
+            image_matrix = [Height,0,0;0,Width,0;0,0,num_images];
+            rand_position_list = rand(num,3) * image_matrix;
+        end
+        function rand_position_list_somaout = rand_position_checker(obj,i,rpl)
+            num = size(rpl,1);
+            soma_images = obj.get_soma_mask(i);
+            [num_images,Height,Width] = obj.get_stack_info(i);
+            for j = 1:num
+                while soma_images(ceil(rpl(j,1)),ceil(rpl(j,2)),ceil(rpl(j,3))) == 1
+                    new_pos = rand(1,3).*[Height,Width,num_images];
+                    rpl(j,:) = new_pos;
                 end
             end
-            ratio = count_nearby/count_all;
+            rand_position_list_somaout = rpl;
         end
-        
-        function ratios = get_paired_ratio(obj,infile,thre)
-            indata = obj.get_indata(infile);
-            ratios = zeros(18,1);
+        function obj = init_rand_DB(obj)
+            obj.All_DB_rand = obj.All_DB;
+            obj.Pos_DB_rand = obj.Pos_DB;
+            obj.Neg_DB_rand = obj.Neg_DB;
+            obj.Pos_multi_DB_rand = obj.Pos_multi_DB;
+            obj.Pos_single_DB_rand = obj.Pos_single_DB;
+            obj.Neg_multi_DB_rand = obj.Neg_multi_DB;
+            obj.Neg_single_DB_rand = obj.Neg_single_DB;
+        end
+        function obj = push_to_randDB(obj,i,DB_name,rpl)
+            new_DB_name = [DB_name,'_rand'];
+            new_DB = obj.(new_DB_name);
+            Cur_name = obj.Sample_name_list{i};
+            Cur_ID = string(cell2mat(new_DB.('Sample'))) == string(Cur_name);
+            new_DB{Cur_ID,6:8} = rpl;
+            obj.(new_DB_name) = new_DB;
+        end
+        function num = get_curr_sample_pl_length(obj,DB_name,i)
+            Cur_name = obj.Sample_name_list{i};
+            Cur_ID = string(cell2mat(obj.(DB_name).('Sample'))) == string(Cur_name);
+            num = numel(find(Cur_ID));
+        end
+        function obj = rpl_generator(obj,type)
             for i = 1:18
-                the_array = obj.get_array(indata,i,4:13);
-                ratios(i) = obj.check_paired_ratio(the_array,thre);
+                num = obj.get_curr_sample_pl_length('All_DB',i);
+                All_DB_rpl = get_rand_positions(obj,i,num);
+                num = obj.get_curr_sample_pl_length('Pos_DB',i);
+                Pos_DB_rpl = get_rand_positions(obj,i,num);
+                num = obj.get_curr_sample_pl_length('Neg_DB',i);
+                Neg_DB_rpl = get_rand_positions(obj,i,num);
+                num = obj.get_curr_sample_pl_length('Pos_multi_DB',i);
+                Pos_multi_DB_rpl = get_rand_positions(obj,i,num);
+                num = obj.get_curr_sample_pl_length('Pos_single_DB',i);
+                Pos_single_DB_rpl = get_rand_positions(obj,i,num);
+                num = obj.get_curr_sample_pl_length('Neg_multi_DB',i);
+                Neg_multi_DB_rpl = get_rand_positions(obj,i,num);
+                num = obj.get_curr_sample_pl_length('Neg_single_DB',i);
+                Neg_single_DB_rpl = get_rand_positions(obj,i,num);
+                if type==2
+                    %do soma exclusive
+                    All_DB_rpl = obj.rand_position_checker(i,All_DB_rpl);
+                    Pos_DB_rpl = obj.rand_position_checker(i,Pos_DB_rpl);
+                    Neg_DB_rpl = obj.rand_position_checker(i,Neg_DB_rpl);
+                    Pos_multi_DB_rpl = obj.rand_position_checker(i,Pos_multi_DB_rpl);
+                    Pos_single_DB_rpl = obj.rand_position_checker(i,Pos_single_DB_rpl);
+                    Neg_multi_DB_rpl = obj.rand_position_checker(i,Neg_multi_DB_rpl);
+                    Neg_single_DB_rpl = obj.rand_position_checker(i,Neg_single_DB_rpl);
+                end
+                obj = obj.push_to_randDB(i,'All_DB',All_DB_rpl);
+                obj = obj.push_to_randDB(i,'Pos_DB',Pos_DB_rpl);
+                obj = obj.push_to_randDB(i,'Neg_DB',Neg_DB_rpl);
+                obj = obj.push_to_randDB(i,'Pos_multi_DB',Pos_multi_DB_rpl);
+                obj = obj.push_to_randDB(i,'Pos_single_DB',Pos_single_DB_rpl);
+                obj = obj.push_to_randDB(i,'Neg_multi_DB',Neg_multi_DB_rpl);
+                obj = obj.push_to_randDB(i,'Neg_single_DB',Neg_single_DB_rpl);
             end
         end
     end
