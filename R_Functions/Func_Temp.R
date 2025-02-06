cba6cde3f83a3919244fdb9b9fd0717648bb610d
ks374 <chenghaz@terpmail.umd.edu>
2025-02-05 16:01:05 -0800

    temp function for statistical power cal

--- R_Functions/Func_Temp.R
+++ R_Functions/Func_Temp.R
@@ -4,3 +4,14 @@ get_power_from_mixedlinear_result <- function(diff,se,df){
     d <- diff/sd
     pwr.t.test(d = d, n = n, sig.level = 0.05, type = "paired", alternative = "greater")
 }
+
+KS_Power_test <- function(statistics,N){
+  d = statistics/(N-1)
+  print(d)
+  pwr.anova.test(
+    k = 2,
+    n = N,
+    f = sqrt(d/(1-d)),
+    sig.level = 0.05
+  )
+}
